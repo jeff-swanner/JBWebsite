6002a0d8346c45df90b687a9c0f4212997ff9516
Jeff Swanner <jeffswanner93@gmail.com>
2019-12-28 00:55:11 -0500

    changes to calendar

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,8 +1,14 @@
 $( document ).ready(function() {
+    if ($('#calendar').width()<440) {
+        $('#calendar2').show();
+    } else {
+        $('#calendar1').show();
+    };
     $("iframe").width($('#calendar').width());
     if ($('#calendar').width()<600) {
         $("iframe").height($('#calendar').width())
     };
+    
     $(window).on('resize', function(){
         $("iframe").width($('#calendar').width());
         if ($('#calendar').width()<600) {
